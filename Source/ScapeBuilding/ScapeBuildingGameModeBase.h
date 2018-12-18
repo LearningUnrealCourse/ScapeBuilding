6978b1eb25a74c71a46b5f8e2d7c20f2158e522e
Albert Alma <albert.alse@gmail.com>
2018-12-18 17:14:37 +0100

    First commit with .gitignore and room created

---
+++ Source/ScapeBuilding/ScapeBuildingGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ScapeBuildingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCAPEBUILDING_API AScapeBuildingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
